06b9265b1ae7f016982bb6f9c5ff69cf56f49513
Samual Norman <samualtnorman@gmail.com>
2023-05-25 21:53:31 +0100

    minify html

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,30 @@
+import htmlMinifierTerser_ from "vite-plugin-html-minifier-terser"
+
+const htmlMinifierTerser = htmlMinifierTerser_.default
+
 /** @type {import("vite").UserConfig} */
-export default { base: "" }
+export default {
+	base: "",
+	plugins: [
+		htmlMinifierTerser(/** @type {import("html-minifier-terser").Options} */ ({
+			collapseBooleanAttributes: true,
+			decodeEntities: true,
+			html5: true,
+			minifyCSS: true,
+			minifyJS: true,
+			processConditionalComments: true,
+			removeAttributeQuotes: true,
+			removeComments: true,
+			removeEmptyAttributes: true,
+			removeOptionalTags: true,
+			removeRedundantAttributes: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true,
+			removeTagWhitespace: true,
+			sortAttributes: true,
+			sortClassName: true,
+			trimCustomFragments: true,
+			useShortDoctype: true
+		}))
+	]
+}
