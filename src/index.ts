5e0f1062cd45334e492bc2ae946b5fb185eae760
Maxwell MacNeill <29025327+Maxwelldoug@users.noreply.github.com>
2026-07-28 11:56:39 -0300

    fix off by 1 error (#198)

--- src/index.ts
+++ src/index.ts
@@ -711,7 +711,7 @@ updateSize()
 function setup() {
 	const tileValues = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
 	const toSet = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ]
-	const levelStats = levels[level][Math.floor(Math.random() * 5)]
+	const levelStats = levels[level - 1][Math.floor(Math.random() * 5)]
 
 	copyMemoMode = false
 	sMemoButton.src = srcButtonMemoSOff
